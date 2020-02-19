c441a73912639ac73e305c947f2ffa12472b2683
cfgs1-01 <cfgs1-01@172.16.15.56>
2020-02-19 13:40:18 +0100

    commit inicial

--- src/pruebaversionin/PruebaVersionin.java
+++ src/pruebaversionin/PruebaVersionin.java
@@ -16,6 +16,7 @@ public class PruebaVersionin {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hola clase");
     }
     
 }
